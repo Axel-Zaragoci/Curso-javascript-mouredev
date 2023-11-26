ffaf86222349635e69b514f0f2eed59af47acf5b
Axel Zaragoci <axelzaragoci@gmail.com>
2023-11-26 13:03:01 +0100

    Creo comentarios de línea única y varias líneas, una variable y muestro la variable en la terminal

--- hola_mundo.js
+++ hola_mundo.js
@@ -1,1 +1,11 @@
+/*
+Esto es un comentario de varias líneas
+*/
+
+// Esto es un comentario de una línea
+
 console.log("Hola mundo");
+
+//Variables
+var myString = "Esto es una cadena de texto"
+console.log(myString)
